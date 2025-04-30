6ab90f520012f5be8361d08c773b77f4110b4a42
SantagatiLuca <lucasantagati03@gmail.com>
2025-04-30 17:51:25 +0200

    gui init

--- gui.py
+++ gui.py
@@ -1,19 +1,48 @@
-from PySide6.QtWidgets import QApplication, QWidget
-
-# Only needed for access to command line arguments
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QFileDialog, QDialog
+from src.layout_colorwidget import Color
 import sys
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("ECG app")
+        self.setMinimumSize(QSize(400,300))
 
-# You need one (and only one) QApplication instance per application.
-# Pass in sys.argv to allow command line arguments for your app.
-# If you know you won't use command line arguments QApplication([]) works too.
+        label = QLabel("Hello!")
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        self.setCentralWidget(label)
+
+        button_action = QAction("&Save", self)
+        button_action.triggered.connect(self.menu_save_clicked)
+        button_action.setCheckable(False)
+
+        button_action2 = QAction("&Open",self)
+        button_action2.triggered.connect(self.menu_open_clicked)
+        button_action2.setCheckable(False)
+
+        menu = self.menuBar()
+
+        file_menu = menu.addMenu("&File")
+        file_menu.addAction(button_action)
+        file_menu.addAction(button_action2)
+ 
+
+    def menu_save_clicked(self):
+        print("Saving file...")
+
+    def menu_open_clicked(self):
+        print("Open file: ...")
+        file_name, _ = QFileDialog.getOpenFileName(self, 'Open File', '', 'All files (*)')
+        if file_name:
+            print(f'Selected file: {file_name}')
+
+#------------------------------------------------------------------------------
 app = QApplication(sys.argv)
-
-# Create a Qt widget, which will be our window.
-window = QWidget()
-window.show()  # IMPORTANT!!!!! Windows are hidden by default.
+window = MainWindow()
+window.show()
 
 # Start the event loop.
-app.exec()
-
-# Your application won't reach here until you exit and the event
-# loop has stopped.
+if __name__ == '__main__':
+    app.exec()
